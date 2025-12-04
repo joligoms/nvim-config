ee5983abdb3c55a41dc2af9b12ae400b7a7edebd
João Gomes <joao.linhares.gomes@accenture.com>
2025-12-04 19:22:57 +0000

    feat: replace 'lazyvim' dashboard title for 'neovim'

---
+++ lua/plugins/ui.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "snacks.nvim",
+    opts = {
+      dashboard = {
+        preset = {
+          header = [[
+█▄ █ ██▀ ▄▀▄ █ █ █ █▄ ▄█
+█ ▀█ █▄▄ ▀▄▀ ▀▄▀ █ █ ▀ █
+          ]],
+        },
+      },
+    },
+  },
+}
