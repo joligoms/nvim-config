cf243c94a5730845a07b1ebf802501dd4af407e9
João L. Gomes <linharesgomes.joao@gmail.com>
2026-04-07 11:25:32 +0100

    eslint

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,16 +3,37 @@ return {
     {
       "neovim/nvim-lspconfig",
       opts = {
-        servers = { eslint = {} },
+        ---@type table<string, vim.lsp.Config>
+        servers = {
+          settings = {
+            eslint = {
+              workingDirectories = { mode = "auto" },
+              format = AUTO_FORMAT,
+            },
+          },
+        },
         setup = {
           eslint = function()
-            Snacks.util.lsp.on(function(_, client)
-              if client.name == "eslint" then
-                client.server_capabilities.documentFormattingProvider = true
-              elseif client.name == "tsserver" then
-                client.server_capabilities.documentFormattingProvider = false
-              end
-            end)
+            if not AUTO_FORMAT then
+              return
+            end
+
+            local formatter = LazyVim.lsp.formatter({
+              name = "eslint: lsp",
+              primary = false,
+              priority = 200,
+              filter = "eslint",
+            })
+
+            LazyVim.format.register(formatter)
+
+            -- Snacks.util.lsp.on(function(_, client)
+            --   if client.name == "eslint" then
+            --     client.server_capabilities.documentFormattingProvider = true
+            --   elseif client.name == "tsserver" then
+            --     client.server_capabilities.documentFormattingProvider = false
+            --   end
+            -- end)
           end,
         },
       },
