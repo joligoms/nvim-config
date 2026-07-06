6d978b88cac7ce87ae7a004ef13140bc6f5fd3ca
João L. Gomes <linharesgomes.joao@gmail.com>
2026-07-06 11:37:28 +0100

    add custom clis

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -13,6 +13,18 @@ end, { desc = "Toggle Horizontal Terminal" })
 
 local CustomCli = require("custom.cli")
 
+map("n", "<leader>h", function()
+  CustomCli.htop()
+end, { desc = "Run Htop" })
+
 map("n", "<leader>p", function()
   CustomCli.posting()
 end, { desc = "Run Posting cli app" })
+
+map("n", "<leader>o", function()
+  CustomCli.opencode()
+end, { desc = "Run OpenCode cli app" })
+
+map("n", "<leader>cx", function()
+  CustomCli.claudecode()
+end, { desc = "Run ClaudeCode cli app" })
---
+++ lua/custom/cli/htop.lua
@@ -0,0 +1,5 @@
+return {
+  cmd = { "htop" },
+  opts = {},
+  terminalOpts = {},
+}
